0f1010a26a2cd8b9a2d73e1151f64176bf0e0f70
ashnayasin12 <ashnayasin12@gmail.com>
2022-07-08 22:44:42 +0530

    search-places output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelRegistrationComponent } from './travel-registration/travel-registration.component';
+import { SearchPlacesComponent } from './search-places/search-places.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelRegistrationComponent
+    TravelRegistrationComponent,
+    SearchPlacesComponent
   ],
   imports: [
     BrowserModule,
