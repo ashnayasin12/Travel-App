023f4daf1cd3e70232a90af7db916e7a0b99b4cc
ashnayasin12 <ashnayasin12@gmail.com>
2022-07-09 00:35:26 +0530

    view-destinations output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelRegistrationComponent } from './travel-registration/travel-registration.component';
 import { SearchPlacesComponent } from './search-places/search-places.component';
+import { ViewDestinationsComponent } from './view-destinations/view-destinations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TravelRegistrationComponent,
-    SearchPlacesComponent
+    SearchPlacesComponent,
+    ViewDestinationsComponent
   ],
   imports: [
     BrowserModule,
